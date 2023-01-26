6b5b3e3825901ce1c7db582d8958bae57f0a165e
Diego Candido <diegocan.sou@gmail.com>
2023-01-25 21:38:17 -0300

    first commit

---
+++ src/repository/carRepository.ts
@@ -0,0 +1,48 @@
+import prisma from "../config/database.js";
+
+async function getCars() {
+  return prisma.cars.findMany();
+}
+
+async function getCar(id: number) {
+  return prisma.cars.findUnique({
+    where: {
+      id
+    }
+  });
+}
+
+async function getCarWithLicensePlate(licensePlate: string) {
+  return prisma.cars.findUnique({
+    where: {
+      licensePlate
+    }
+  })
+}
+
+async function createCar(model: string, licensePlate: string, year: number, color: string) {
+  await prisma.cars.create({
+    data: {
+      model,
+      color,
+      licensePlate,
+      year
+    }
+  })
+}
+
+async function deleteCar(id: number) {
+  await prisma.cars.delete({
+    where: { id }
+  })
+}
+
+const carRepository = {
+  getCar,
+  getCarWithLicensePlate,
+  getCars,
+  createCar,
+  deleteCar
+}
+
+export default carRepository;
